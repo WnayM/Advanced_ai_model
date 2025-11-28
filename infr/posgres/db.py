55782e4bec6770ac0c15fa9142709ec89233cccb
WnayM <s30221@pjwstk.edu.pl>
2025-11-28 13:35:37 +0100

    starting with db

---
+++ infr/posgres/db.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from os import getenv
+from dotenv import load_datenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in .env")
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_engine(
+    DATABASE_URL,
+    echo = False,
+    pool_pre_ping = True
+)
+
+SessionLocal = sessionmaker(
+    bing = engine,
+    autoflush = False,
+    autocommit = False
+)
